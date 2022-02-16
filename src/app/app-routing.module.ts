cbd54ac49163ff6a5e871804df2372b927d6560c
Yoelp <51400150+yoelpater@users.noreply.github.com>
2022-02-16 12:25:04 +0800

    lazy load route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { RouterModule, Routes } from '@angular/router'
 import { HomeComponent } from './home/home.component'
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
-import { managerModuleRoutes } from './manager/manager.module'
-
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'manager', children: managerModuleRoutes },
+  {
+    path: 'manager',
+    loadChildren: () => import('./manager/manager.module').then((m) => m.ManagerModule),
+  },
   { path: '**', component: PageNotFoundComponent },
 ]
 
--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -1,13 +1,11 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
-import { Routes } from '@angular/router'
 
-import { ManagerRoutingModule } from './manager-routing.module'
 import { ManagerHomeComponent } from './manager-home/manager-home.component'
+import { ManagerRoutingModule } from './manager-routing.module'
 
-export const managerModuleRoutes: Routes = [{ path: '', component: ManagerHomeComponent }]
 @NgModule({
   declarations: [ManagerHomeComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ManagerRoutingModule],
 })
 export class ManagerModule {}
