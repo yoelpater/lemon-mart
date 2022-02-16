be19278f980b295f19e61e4bbd1f354712108325
Yoelp <51400150+yoelpater@users.noreply.github.com>
2022-02-16 15:17:55 +0800

    auth service and cache service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { InMemoryAuthService } from './auth/auth.inmemory.service'
+import { AuthService } from './auth/auth.service'
 import { HomeComponent } from './home/home.component'
 import { MaterialModule } from './material.module'
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
@@ -20,7 +22,12 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     MaterialModule,
     FlexLayoutModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: AuthService,
+      useClass: InMemoryAuthService,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,75 @@
+import { decode } from 'punycode'
+
+import { Injectable } from '@angular/core'
+import { catchError, filter, map, mergeMap, tap, throwError } from 'rxjs'
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject'
+import { Observable } from 'rxjs/internal/Observable'
+
+import { transformError } from '../common/common'
+import { IUser, User } from '../user/user/user'
+import { Role } from './auth.enum'
+import { CacheService } from './cache.service'
+
+export interface IAuthStatus {
+  isAuthenticated: boolean
+  userRole: Role
+  userId: string
+}
+export interface IServerAuthResponse {
+  accessToken: string
+}
+export const defaultAuthStatus: IAuthStatus = {
+  isAuthenticated: false,
+  userRole: Role.None,
+  userId: '',
+}
+export interface IAuthService {
+  readonly authStatus$: BehaviorSubject<IAuthStatus>
+  readonly currentUser$: BehaviorSubject<IUser>
+  login(email: string, password: string): Observable<void>
+  logout(clearToken?: boolean): void
+  getToken(): string
+}
+
+@Injectable()
+export abstract class AuthService extends CacheService implements IAuthService {
+  protected abstract authProvider(
+    email: string,
+    password: string
+  ): Observable<IServerAuthResponse>
+  protected abstract transformJwtToken(token: unknown): IAuthStatus
+  protected abstract getCurrentUser(): Observable<User>
+  readonly authStatus$ = new BehaviorSubject<IAuthStatus>(defaultAuthStatus)
+  readonly currentUser$ = new BehaviorSubject<IUser>(new User())
+
+  constructor() {
+    super()
+  }
+
+  login(email: string, password: string): Observable<void> {
+    const loginResponse$ = this.authProvider(email, password).pipe(
+      map((value) => {
+        const token = decode(value.accessToken)
+        return this.transformJwtToken(token)
+      }),
+      tap((status) => this.authStatus$.next(status)),
+      filter((status: IAuthStatus) => status.isAuthenticated),
+      mergeMap(() => this.getCurrentUser()),
+      map((user) => this.currentUser$.next(user)),
+      catchError(transformError)
+    )
+    loginResponse$.subscribe({
+      error: (err) => {
+        this.logout()
+        return throwError(err)
+      },
+    })
+    return loginResponse$
+  }
+  logout(clearToken?: boolean): void {
+    setTimeout(() => this.authStatus$.next(defaultAuthStatus), 0)
+  }
+  getToken(): string {
+    throw new Error('Method not implemented.')
+  }
+}
